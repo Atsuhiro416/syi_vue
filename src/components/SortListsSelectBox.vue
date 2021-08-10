1fcb6484cfb2a991eabd125b8df1a7d63194b343
Atsuhiro416 <myamp012@gmail.com>
2021-08-10 20:46:51 +0900

    リストソート機能追加

---
+++ src/components/SortListsSelectBox.vue
@@ -0,0 +1,55 @@
+<template>
+  <select
+    class="select-box"
+    name="sort"
+    v-model="currentSortId"
+    @change="sortLists"
+  >
+    <option v-for="sort in sorts" :key="sort.id" :value="sort.id">
+      {{ sort.value }}
+    </option>
+  </select>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      currentSortId: 1,
+      sorts: [
+        { id: 1, value: "作成日順：古" },
+        { id: 2, value: "作成日順：新" },
+        { id: 3, value: "更新日順：古" },
+        { id: 4, value: "更新日順：新" },
+      ],
+    };
+  },
+  methods: {
+    sortLists() {
+      this.$emit("sort-lists", this.currentSortId);
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+$sp: 481px;
+
+@mixin sp {
+  @media (max-width: ($sp)) {
+    @content;
+  }
+}
+
+.select-box {
+  border-radius: 5px;
+  padding: 1px 5px;
+  font-size: 14px;
+
+  @include sp {
+    font-size: 12px;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,11 @@
     <logged-header>
       <template #header-name>リスト</template>
     </logged-header>
+
+    <div class="select-boxes">
+      <SortListsSelectBox @sort-lists="sortLists" />
+    </div>
+
     <div class="lists">
       <List v-for="list in lists" :key="list.id" :list="list" />
     </div>
@@ -13,6 +18,7 @@
 import { defineComponent } from "vue";
 import LoggedHeader from "../components/Header.vue";
 import List from "../components/List.vue";
+import SortListsSelectBox from "../components/SortListsSelectBox.vue";
 import listRepository from "../repositories/listRepository";
 
 export default defineComponent({
@@ -33,6 +39,31 @@ export default defineComponent({
           alert("問題が発生しました。");
         });
     },
+
+    sortLists(id: number) {
+      const lists = this.lists;
+
+      if (id === 1) {
+        lists.sort((a: any, b: any) => {
+          return a.created_at < b.created_at ? -1 : 1;
+        });
+      }
+      if (id === 2) {
+        lists.sort((a: any, b: any) => {
+          return a.created_at < b.created_at ? 1 : -1;
+        });
+      }
+      if (id === 3) {
+        lists.sort((a: any, b: any) => {
+          return a.updated_at < b.updated_at ? -1 : 1;
+        });
+      }
+      if (id === 4) {
+        lists.sort((a: any, b: any) => {
+          return a.updated_at < b.updated_at ? 1 : -1;
+        });
+      }
+    },
   },
   created() {
     this.getLists();
@@ -40,6 +71,14 @@ export default defineComponent({
   components: {
     LoggedHeader,
     List,
+    SortListsSelectBox,
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.select-boxes {
+  margin-top: 2vh;
+  margin-left: 8vw;
+}
+</style>
