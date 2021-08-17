8e31d46396ba24e3f1fb22abbba94cc2bed6977b
Atsuhiro416 <myamp012@gmail.com>
2021-08-17 16:40:03 +0900

    リスト表示切替機能追加

---
+++ src/components/ChangeDisplayListsSelectBox.vue
@@ -0,0 +1,53 @@
+<template>
+  <select
+    class="select-box"
+    name="display"
+    v-model="currentDisplayId"
+    @change="changeDisplay"
+  >
+    <option v-for="display in displays" :key="display.id" :value="display.id">
+      {{ display.value }}
+    </option>
+  </select>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      currentDisplayId: 1,
+      displays: [
+        { id: 1, value: "表示のみ" },
+        { id: 2, value: "簡易編集" },
+      ],
+    };
+  },
+  methods: {
+    changeDisplay() {
+      this.$emit("change-display-lists", this.currentDisplayId);
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+$sp: 481px;
+
+@mixin sp {
+  @media (max-width: ($sp)) {
+    @content;
+  }
+}
+
+.select-box {
+  border-radius: 5px;
+  padding: 1px 5px;
+  font-size: 14px;
+
+  @include sp {
+    font-size: 12px;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,13 @@
 
     <div class="select-boxes">
       <SortListsSelectBox @sort-lists="sortLists" />
+      <ChangeDisplayLists @change-display-lists="changeDisplayLists" />
     </div>
 
-    <div class="lists">
-      <!-- <List v-for="list in lists" :key="list.id" :list="list" /> -->
-
+    <div class="lists" v-if="displayListsId === 1">
+      <List v-for="list in lists" :key="list.id" :list="list" />
+    </div>
+    <div class="lists" v-if="displayListsId === 2">
       <EditableList
         v-for="list in lists"
         :key="list.id"
@@ -24,15 +26,17 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import LoggedHeader from "../components/Header.vue";
-// import List from "../components/List.vue";
+import List from "../components/List.vue";
 import EditableList from "../components/EditableList.vue";
 import SortListsSelectBox from "../components/SortListsSelectBox.vue";
+import ChangeDisplayLists from "../components/ChangeDisplayListsSelectBox.vue";
 import listRepository from "../repositories/listRepository";
 
 export default defineComponent({
   data() {
     return {
       lists: [],
+      displayListsId: 1,
     };
   },
   methods: {
@@ -72,15 +76,25 @@ export default defineComponent({
         });
       }
     },
+
+    changeDisplayLists(id: number) {
+      if (id === 1) {
+        this.displayListsId = 1;
+      }
+      if (id === 2) {
+        this.displayListsId = 2;
+      }
+    },
   },
   created() {
     this.getLists();
   },
   components: {
     LoggedHeader,
-    // List,
+    List,
     EditableList,
     SortListsSelectBox,
+    ChangeDisplayLists,
   },
 });
 </script>
