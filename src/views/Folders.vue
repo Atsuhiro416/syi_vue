5447bd1efef63bce862483caf13f4f33df86d44d
Atsuhiro416 <myamp012@gmail.com>
2021-09-03 14:24:27 +0900

    ユーザーのフォルダ一覧取得

--- src/views/Folders.vue
+++ src/views/Folders.vue
@@ -1,3 +1,33 @@
 <template>
   <div class="folders">folders</div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import FoldersRepository from "../repositories/foldersRepository";
+
+export default defineComponent({
+  data() {
+    return {
+      folders: [],
+    };
+  },
+  methods: {
+    getFolders() {
+      FoldersRepository.getUserFolders()
+        .then((res) => {
+          this.folders = res.data.data;
+        })
+        .catch((e) => {
+          console.log(e.resopnse);
+          alert("問題が発生しました。");
+        });
+    },
+  },
+  created() {
+    this.getFolders();
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
