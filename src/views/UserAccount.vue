359962cbeb82b5127ac58c6e931b1d8615ba1dda
Atsuhiro416 <myamp012@gmail.com>
2021-10-26 14:33:22 +0900

    ユーザのメールアドレス、リスト数、フォルダ数取得＋表示

--- src/views/UserAccount.vue
+++ src/views/UserAccount.vue
@@ -1,3 +1,151 @@
 <template>
-  <div class="account">account</div>
+  <div class="account">
+    <SideMenu />
+
+    <div class="account-main">
+      <logged-header>
+        <template #header-name>アカウント</template>
+      </logged-header>
+
+      <div class="account-info">
+        <p class="account-info__detail">メールアドレス：{{ userEmail }}</p>
+        <p class="account-info__detail">
+          リスト数：{{ numberWithDigits(counts.list) }}
+        </p>
+        <p class="account-info__detail">
+          フォルダ数：{{ numberWithDigits(counts.folder) }}
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import store from "@/store";
+import LoggedHeader from "../components/Header.vue";
+import SideMenu from "../components/SideMenu.vue";
+import listRepository from "../repositories/listRepository";
+import foldersRepository from "../repositories/foldersRepository";
+
+export default defineComponent({
+  data() {
+    return {
+      userEmail: "",
+      counts: {
+        list: 0,
+        folder: 0,
+      },
+    };
+  },
+  methods: {
+    getUserInfo() {
+      const user = store.getters.getUserInfo;
+      this.userEmail = user.email;
+    },
+
+    getListCounts() {
+      listRepository
+        .getUserListCounts()
+        .then((res) => {
+          this.counts.list = res.data.counts;
+        })
+        .catch((e) => {
+          const error = e.response;
+          alert(
+            `${error.status} ${error.statusText}\n問題が発生しました。\nもう一度操作してください。`
+          );
+        });
+    },
+    getFolderCounts() {
+      foldersRepository
+        .getUserFolderCounts()
+        .then((res) => {
+          this.counts.folder = res.data.counts;
+        })
+        .catch((e) => {
+          const error = e.response;
+          alert(
+            `${error.status} ${error.statusText}\n問題が発生しました。\nもう一度操作してください。`
+          );
+        });
+    },
+  },
+  computed: {
+    //数字を三桁ごとにカンマ区切りにする
+    numberWithDigits() {
+      return (num: number) => {
+        return num.toLocaleString("ja-JP");
+      };
+    },
+  },
+  created() {
+    this.getUserInfo();
+    this.getListCounts();
+    this.getFolderCounts();
+  },
+  components: {
+    LoggedHeader,
+    SideMenu,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+$pc: 1024px;
+$tab: 769px;
+$sp: 481px;
+
+@mixin pc {
+  @media (max-width: ($pc)) {
+    @content;
+  }
+}
+
+@mixin tab {
+  @media (max-width: ($tab)) {
+    @content;
+  }
+}
+
+@mixin sp {
+  @media (max-width: ($sp)) {
+    @content;
+  }
+}
+
+.account {
+  width: 100%;
+  display: grid;
+  grid-template-rows: auto;
+  grid-template-columns: 15% 85%;
+  grid-template-areas: "nav main";
+
+  @include pc {
+    grid-template-columns: 20% 80%;
+  }
+  @include tab {
+    grid-template-areas: "main main";
+  }
+
+  &-main {
+    grid-area: main;
+  }
+  &-nav {
+    grid-area: nav;
+  }
+
+  &-info {
+    margin: 10vh 10vw;
+    font-size: 1.4rem;
+
+    &__detail {
+      margin-bottom: 20px;
+      line-height: 1.25;
+      overflow-wrap: break-word;
+      padding-left: 1.2em;
+      text-indent: -1.2em;
+    }
+  }
+}
+</style>
